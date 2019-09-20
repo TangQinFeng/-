4697041ab82b1257d252805f069ab36fe46d9ea6
tang <979472293@qq.com>
2019-09-20 21:16:55 +0800

    导航守卫完成

---
+++ src/permission.js
@@ -0,0 +1,16 @@
+import router from './router'
+
+router.beforeEach((to, from, next) => {
+  if (to.path.startsWith('/home')) {
+    let token = window.localStorage.getItem('login-token')
+    if (token) {
+      next()
+    } else {
+      next('/login')
+    }
+  } else {
+    next()
+  }
+})
+
+export default router
